e2e461f12eb5f5282c6bcf4bdda00a4d24955471
Lynlee21 <96206081+Lynlee21@users.noreply.github.com>
2022-02-05 12:35:55 -0600

    Adjusted HTML and CSS, added JS

--- src/app.js
+++ src/app.js
@@ -27,3 +27,50 @@ function formatDay(timestamp) {
 
   return days[day];
 }
+function getForecast(coordinates) {
+  let apiKey = "470b2457f49a129b497496a403990894";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat={coordinates.lat}&lon={coordinates.lon}&appid={apiKey}`;
+}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = reponse.data.main.temperatureElement;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = reponse.data.name;
+  descriptionElement = reponse.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#cityInput");
+  console.log(searchInput);
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${searchInput.value}`;
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#cityInput");
+  let searchCity = `${cityInput.value}`;
+  let apiKey = "470b2457f49a129b497496a403990894";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(retrieveWeatherData);
+}
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", searchCity);
